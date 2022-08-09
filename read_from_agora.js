477bdca19debaa0f5ebf87302832cca2a9b8a7dc
Mixarium <106687030+Mixarium@users.noreply.github.com>
2022-08-09 19:05:36 +0300

    Delete read_from_agora.js

--- read_from_agora.js
+++
@@ -1,8 +0,0 @@
-fetch('https://moldovan-content-aggregator-json-api.mixarium.repl.co/agora_json')
-  .then(function (response) {
-    let li = document.createElement('li')
-    li.innerText = response;
-    list.appendChild(li);
-  })
-  .catch(function (err) {
-  });
