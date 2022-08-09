2157a987665def7487ac6b5f40f522de6df5ed6f
Mixarium <106687030+Mixarium@users.noreply.github.com>
2022-08-09 19:05:24 +0300

    Add files via upload

---
+++ scripts/read_from_stirimd.js
@@ -0,0 +1,31 @@
+function display_latest(amount) {
+  console.log('script started')
+    fetch("https://moldovan-content-aggregator-json-api.mixarium.repl.co/stirimd_json")
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR")
+      }
+    })
+    .then(data => {
+      let list = document.getElementById('latest_stirimd_news')
+    if (amount !== undefined) {
+      length = amount
+    } else {
+      length = Object.keys(data).length
+    }
+    for (let i = 0; i < length; i++) {
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+        a.className = "news_link"
+        a.href = data[i]['link']
+        a.text = data[i]['title']
+        li.appendChild(a)
+        list.appendChild(li)
+    }
+    })
+    .catch((error) => console.error("FETCH ERROR:", error))
+}
+
+display_latest(10)
